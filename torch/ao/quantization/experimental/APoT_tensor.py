f61770fb780ee9dbf8c17e8a3a1b51a345a411b3
asl3 <amandaliu@fb.com>
2022-06-21 10:50:44 -0700

    [quant] Implement APoT_tensor class
    ghstack-source-id: 01ea95179bf015c628f378d88182f4d706ad8c1a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79940

--- torch/ao/quantization/experimental/APoT_tensor.py
+++ torch/ao/quantization/experimental/APoT_tensor.py
@@ -1,14 +1,27 @@
 import torch
 from torch.ao.quantization.experimental.quantizer import APoTQuantizer
+from torch.ao.quantization.experimental.apot_utils import float_to_apot
 
 # class to store APoT quantized tensor
 class TensorAPoT(torch.Tensor):
     quantizer: APoTQuantizer
-    data_reduced_precision_repr: torch.Tensor
-    data_int_repr: torch.Tensor
+    data: torch.Tensor
+    use_int_repr: bool
 
-    def __init__(self, quantizer):
-        raise NotImplementedError
+    def __init__(self, quantizer: APoTQuantizer):
+        self.quantizer = quantizer
+        self.data = quantizer.data
+        self.use_int_repr = quantizer.use_int_repr
 
     def int_repr(self):
-        raise NotImplementedError
+        if self.use_int_repr:
+            return self.quantizer.data
+        else:
+            data_copy = torch.clone(self.data)
+            # convert to int representation
+            quantizer_int_repr = data_copy.apply_(lambda x:
+                                                  float_to_apot(x,
+                                                                self.quantizer.quantization_levels,
+                                                                self.quantizer.level_indices))
+
+            return quantizer_int_repr
